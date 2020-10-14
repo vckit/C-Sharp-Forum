73d3cf53316407beb0cf13d3b4008788f2d14836
Abdulkhakim <thevckit@gmail.com>
2020-10-14 05:03:51 +0300

    WPF Folder Edit

---
+++ WPF/OLD`S/C-Sharp-Forum3/C-Sharp-Forum3/Classes/dbLoading.cs
@@ -0,0 +1,31 @@
+﻿using C_Sharp_Forum3.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace C_Sharp_Forum3.Classes
+{
+    class dbLoading
+    {
+        public void LoadCountry(ComboBox comboBox)
+        {
+            try
+            {
+                var query = dbContext.db.CountryManufacture.Select(item => new
+                {
+                    ID = item.Title
+                });
+                var collection = from item in query select item.ID;
+                comboBox.ItemsSource = collection.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
