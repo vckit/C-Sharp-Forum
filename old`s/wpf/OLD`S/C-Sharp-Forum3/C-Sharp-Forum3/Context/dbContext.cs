e8937fff8ea02449522cd0bc74c8d5f8394ab050
Abdulkhakim Magomedov <thevckit@gmail.com>
2020-11-29 22:48:00 +0300

    RUN Courses 11.29.2020

--- old`s/wpf/OLD`S/C-Sharp-Forum3/C-Sharp-Forum3/Context/dbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using C_Sharp_Forum3.Models;
-
-namespace C_Sharp_Forum3.Context
-{
-    public class dbContext
-    {
-        public static dbCSharpForum4Entities db = new dbCSharpForum4Entities();
-    }
-}
