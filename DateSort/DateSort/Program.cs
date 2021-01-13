bfae9d25efefee692ff3b632c5e51c63b6ec5428
Abdulkhakim Magomedov <thevckit@gmail.com>
2021-01-13 16:50:07 +0300

    Отображение даты в разных форматах

---
+++ DateSort/DateSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DateSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("D: " + now.ToString("D"));
+            Console.WriteLine("d: " + now.ToString("d"));
+            Console.WriteLine("F: " + now.ToString("F"));
+            Console.WriteLine("f: {0:f}", now);
+            Console.WriteLine("G: {0:G}", now);
+            Console.WriteLine("g: {0:g}", now);
+            Console.WriteLine("M: {0:M}", now);
+            Console.WriteLine("O: {0:O}", now);
+            Console.WriteLine("o: {0:o}", now);
+            Console.WriteLine("R: {0:R}", now);
+            Console.WriteLine("s: {0:s}", now);
+            Console.WriteLine("T: {0:T}", now);
+            Console.WriteLine("t: {0:t}", now);
+            Console.WriteLine("U: {0:U}", now);
+            Console.WriteLine("u: {0:u}", now);
+            Console.WriteLine("Y: {0:Y}", now);
+            Console.ReadKey();
+        }
+    }
+}
