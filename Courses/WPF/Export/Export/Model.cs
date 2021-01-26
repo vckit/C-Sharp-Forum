6fb68fe4e6cd0f07c32edd290f9716f37f1d7d96
vckit <thevcit@gmail.com>
2021-01-26 16:36:14 +0300

    Export to .CSV

---
+++ Courses/WPF/Export/Export/Model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Export
+{
+ 
+    class Model
+    {
+        private IEnumerable<Model> _rows;
+        public IEnumerable<Model> Rows
+        {
+            get { return _rows; }
+            set
+            {
+                _rows = value;
+                OnPropertyChanged("Rows");
+            }
+        }
+
+        private void OnPropertyChanged(string v)
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
